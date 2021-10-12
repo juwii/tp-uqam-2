a187d8e269e5d7963c10b4eba2f9076f52a0aa83
Létondot, Juliette <letondot.juliette@courrier.uqam.ca>
2021-10-12 19:07:47 -0400

    initialisation code

---
+++ cesar.c
@@ -0,0 +1,27 @@
+/**
+ * TP2 : décryption du chiffrement de César
+ * 
+ * Le projet consiste en le déchiffrage d'un texte d'un fichier en entree standard
+ * le texte en clair est enregistré dans un fichier en sortie standard
+ * @author  Juliette Létondot
+ */
+
+
+/* CONTRAINTES
+indentation de 3 espaces
+fonctions pas plus de 10 lignes
+gerer erreurs systèmes
+identifiants en snake case
+*/
+
+// import
+#include <stdio.h>
+
+
+// définition des fonctions
+
+// main
+int main(int argc, char *argv[]){
+    printf("test\n");
+    return 0;
+}
